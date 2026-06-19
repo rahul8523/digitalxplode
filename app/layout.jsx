3c80708db5d05ba06b13947714f8d5b8ecc9126b
Rahul Singh <123927740+rahul8523@users.noreply.github.com>
2026-06-19 16:29:00 +0530

    Update layout.jsxchanges

--- app/layout.jsx
+++ app/layout.jsx
@@ -9,6 +9,16 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <script async src="https://www.googletagmanager.com/gtag/js?id=AW-18249413643"></script>
+        <script>
+          {`window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'AW-18249413643');`}
+        </script>
+      </head>
       <body>
         {children}
         <Footer />
