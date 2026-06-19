7ab6a6862be749e451bf2f333395c465426f55b7
Rahul Singh <123927740+rahul8523@users.noreply.github.com>
2026-06-19 16:33:16 +0530

    Update layout.jsx

--- app/layout.jsx
+++ app/layout.jsx
@@ -10,16 +10,33 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-        <script async src="https://www.googletagmanager.com/gtag/js?id=AW-18249413643"></script>
-        <script dangerouslySetInnerHTML={{ __html: `window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-          gtag('config', 'AW-18249413643');
-          gtag('event', 'conversion', {
-            'send_to': 'AW-18249413643/gmHSCMHqgsEcEIvo__1D',
-            'value': 1.0,
-            'currency': 'INR'
-          });` }} />
+         {/* Google Ads Tag */}
+    <Script
+      src="https://www.googletagmanager.com/gtag/js?id=AW-18249413643"
+      strategy="afterInteractive"
+    />
+
+    <Script id="google-ads-conversion" strategy="afterInteractive">
+      {`
+        window.dataLayer = window.dataLayer || [];
+
+        function gtag(){
+          dataLayer.push(arguments);
+        }
+
+        window.gtag = gtag;
+
+        gtag('js', new Date());
+
+        gtag('config', 'AW-18249413643');
+
+        gtag('event', 'conversion', {
+          send_to: 'AW-18249413643/gmHSCMHqgsEcEIvo__1D',
+          value: 1.0,
+          currency: 'INR'
+        });
+      `}
+    </Script>
       </head>
       <body>
         {children}
