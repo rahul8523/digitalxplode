f3727848f64cafec4cfbb92e088d3831b71eff34
Rahul Singh <123927740+rahul8523@users.noreply.github.com>
2026-07-06 10:58:35 +0530

    SS

--- app/layout.jsx
+++ app/layout.jsx
@@ -10,16 +10,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-        <script async src="https://www.googletagmanager.com/gtag/js?id=AW-18249413643"></script>
-        <script dangerouslySetInnerHTML={{ __html: `window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-          gtag('config', 'AW-18249413643');
-          gtag('event', 'conversion', {
-            'send_to': 'AW-18249413643/gmHSCMHqgsEcEIvo__1D',
-            'value': 1.0,
-            'currency': 'INR'
-          });` }} />
+     
       </head>
       <body>
         {children}
