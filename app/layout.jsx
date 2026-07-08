22fa9e9dd3a16b241e3ecfbc62468602213d52c6
Rahul Singh <123927740+rahul8523@users.noreply.github.com>
2026-07-08 11:42:26 +0530

    Update layout.jsx

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,6 +1,6 @@
 import '../styles/globals.css';
 import Footer from '@/components/Footer';
-
+import Script from "next/script";
 export const metadata = {
   title: 'Digital Xplode - Leading Digital Marketing Agency for Manufacturing Companies',
   description: 'Fast Next.js landing page with sticky header and responsive hero',
@@ -10,7 +10,20 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-     
+          <Script
+          src="https://www.googletagmanager.com/gtag/js?id=AW-18249413643"
+          strategy="afterInteractive"
+        />
+
+        <Script id="google-ads" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            window.gtag = gtag;
+            gtag('js', new Date());
+            gtag('config', 'AW-18249413643');
+          `}
+        </Script>
       </head>
       <body>
         {children}
